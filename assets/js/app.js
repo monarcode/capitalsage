75f11c77913e7415437000bdebfc95f12f540c02
monarcode <ugbahisioma@gmail.com>
2020-08-27 08:10:10 +0100

    v3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('.carousel').carousel()
+    $('.carousel').carousel();
+
+    $(window).scroll(function(){
+        let scr = $(window).scrollTop();
+
+        // $("#nav").toggleClass("nav-scroll",src>20);
+        if(scr>20){
+            $('#nav').toggleClass('nav-scroll');
+        }
+    })
 })
